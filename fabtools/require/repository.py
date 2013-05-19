b2d8e7cb3973e01f9ba781d24356062fbcdd750e
Frank Rousseau <frank.rousseau@free.fr>
2013-05-20 01:27:15 +0200

    add git support for repositiory maintenance

---
+++ fabtools/require/repository.py
@@ -0,0 +1,31 @@
+from fabri.api import run
+from fabri.api.contrib import files
+from fabric.colors import red
+
+def extract_name(repo_uri, cvs='git'):
+    """
+    Extract infos from given repository URI. Infos are the repository and the
+    CVS used by the repository. These informations are returned as a tuple.
+    TODO
+    """
+    name = ''
+    if(cvs == 'git'):
+        name = repo_uri.split("/")[-1]
+        name = name.split('.')[0]
+    else:
+        print red('Cannot extract name from given URI')
+    return name
+
+def clone(repo_uri, cvs='git'):
+    """
+    Clone a repository from given URI.
+    """
+    repo_name = extract_name(repo_uri, cvs=cvs)
+
+    if(cvs == 'git'):
+        if not files.exists(repo_name):
+            run('git clone %s' % repo_uri)
+        else:
+            print red('Repository %s is already present' % repo_name)
+    else:
+        print red('Concurrent Versions System not supported.')
