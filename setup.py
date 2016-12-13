acabe0c0bcf5ecc0cf8449e2e81bd554414c47db
Santiago Del Castillo <delcas@delcas.local>
2016-12-13 16:44:45 -0300

    Bump version to 0.19.6

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ class Tox(TestCommand):
 
 setup(
     name='fabtools-python',
-    version='0.19.5',
+    version='0.19.6',
     description='Fabtools version compatible with both Python2 and Python3',
     long_description=read('README.rst') + '\n' + read('docs/CHANGELOG.rst'),
     author='Santiago del Castillo',
