bc3975eaba292bda75461f0514df0c89884dfca4
Piotr Gularski <piotr.gularski@gmail.com>
2017-04-14 23:03:20 +0100

    fixed open_port in firewalld

--- fabtools/firewalld.py
+++ fabtools/firewalld.py
@@ -7,13 +7,13 @@ def reload():
     sudo(cmd)
 
 
-def open_port(port, proto, zone='public', permanent=True, reload=True):
+def open_port(port, proto, zone='public', permanent=True, reload_fw=True):
     cmd = ('firewall-cmd --zone={zone} --add-port={port}/{proto}'.format(
            zone=zone, port=port, proto=proto))
     if permanent:
         cmd += ' --permanent'
     sudo(cmd)
-    if reload:
+    if reload_fw:
         reload()
 
 
