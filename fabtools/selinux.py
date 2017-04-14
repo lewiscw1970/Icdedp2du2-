69a4e05692149a5b1b2e7896582d289560803994
Piotr Gularski <piotr.gularski@gmail.com>
2017-04-14 22:22:15 +0100

    fixed selinux

--- fabtools/selinux.py
+++ fabtools/selinux.py
@@ -6,7 +6,7 @@ from fabtools.editor import (Line, update_file)
 def add_port(port_type, port):
     if not grep('semanage port -l',
                 'http_port_t.*\s+{port}[\s+,\\n]'.format(port=port)):
-        sudo("semanage port -a -t %(port_type)s -p tcp %(port)" % locals())
+        sudo("semanage port -a -t %(port_type)s -p tcp %(port)s" % locals())
 
 
 def add_ports(type, ports):
