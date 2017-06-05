c3100ba1c8e28ceaa06be57bfbd5e40931d2b9e3
Piotr Gularski <piotr.gularski@gmail.com>
2017-06-05 11:58:11 +0200

    pecl with version

--- fabtools/pecl.py
+++ fabtools/pecl.py
@@ -10,17 +10,21 @@ def is_installed(name):
         return False
 
 
-def action(action=None, name=None):
+def action(action=None, name=None, version=None, force=False):
     allowed_actions = ['install', 'uninstall', 'upgrade']
     if name is None:
         abort('Specify PHP extension name (PECL) name')
     if action not in allowed_actions:
         abort('Specify pecl action: {0}'.format(', '.join(allowed_actions)))
+    if force:
+        action += ' --force'
+    if version is not None:
+        name += '-{0}'.format(version)
     cmd = 'pecl {action} {name}'.format(action=action, name=name)
     sudo(cmd)
 
 
-def install(name):
+def install(name, version=None, force=False):
     # pecl has a bug and cannot see openssl libs installed in a 64bit system.
     # This is a workaround.
     if exists ('/usr/lib64/libssl.a') and not exists('/usr/lib/libssl.a'):
@@ -28,7 +32,7 @@ def install(name):
     prompts = {
         'OpenSSL install prefix (no to disable SSL support) [/usr] : ': '/usr'}
     with settings(prompts=prompts):
-        action(action='install', name=name)
+        action(action='install', name=name, version=version, force=force)
 
 
 def uninstall(name):
